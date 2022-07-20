0f2e5b08880f7339c91cef867a1b996939d27b9b
paulmcmurray1978 <paul.mcmurray@accenture.com>
2022-07-20 12:47:15 +0100

    first push

---
+++ problems-1.js
@@ -0,0 +1,15 @@
+/*
+    This function should take an array of integers, and return one of the following:
+
+    "yes, ascending" - if the numbers in the array are sorted in an ascending order
+    "yes, descending" - if the numbers in the array are sorted in a descending order
+    "no" - otherwise
+*/
+
+function isSortedAndHow(nums) {
+    
+}
+
+// Do not modify this code
+window.solutions = window.solutions || {}
+window.solutions.isSortedAndHow = isSortedAndHow
---
+++ problems-2.js
@@ -0,0 +1,13 @@
+/*
+    Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+
+    For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+*/
+
+function sumTwoSmallestNumbers(nums) {
+    
+}
+
+// Do not modify this code
+window.solutions = window.solutions || {}
+window.solutions.sumTwoSmallestNumbers = sumTwoSmallestNumbers
